827a74a9a938e2f89a3b970b7cdd2c02464a51ab
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-05-30 10:50:34 -0300

    Create pilha.h

---
+++ Aula 13/Atividade 2/pilha.h
@@ -0,0 +1,19 @@
+#define MAX 100
+
+typedef struct aluno {
+    int matricula;
+    float n1, n2, n3;
+} ALUNO;
+
+typedef struct pilha *PILHA;
+
+PILHA *criaPilha(); // Corrigido
+
+void liberaPilha(PILHA *pi);
+int tamanhoPilha(PILHA *pi);
+int pilhaCheia(PILHA *pi);
+int pilhaVazia(PILHA *pi);
+int inserePilha(PILHA *pi, ALUNO al);
+int removePilha(PILHA *pi);
+int acessaTopoPilha(PILHA *pi, ALUNO *al);
+
