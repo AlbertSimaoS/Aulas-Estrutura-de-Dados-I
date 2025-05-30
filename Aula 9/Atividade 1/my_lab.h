ab9abfa8b3ac22abf2deb4ee04f6d88124d33c6e
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2025-05-30 10:28:32 -0300

    Create my_lab.h

---
+++ Aula 9/Atividade 1/my_lab.h
@@ -0,0 +1,3 @@
+void calculoVantagens(float numeroHoras, float salarioHora, float numeroFilhos, float valorPorFilho, float *salarioBruto, float *salarioFamilia, float *vantagens);
+
+void calculoDeducoes(float salarioBruto, float taxaIR, float *INSS, float *IPRF, float *deducoes);
